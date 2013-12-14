6b816422bca3fa1f4cfec88a611fc10b6b50021a
Brett Hollman <brett_hollman@yahoo.com>
2013-12-13 16:05:07 -0800

    updated knife.rb and index.html

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -14,3 +14,4 @@ cookbook_path            ["#{current_dir}/../cookbooks"]
 knife[:aws_access_key_id] = "#{ENV['AWS_ACCESS_KEY_ID']}"
 knife[:aws_secret_access_key] = "#{ENV['AWS_SECRET_ACCESS_KEY']}"
 knife[:aws_ssh_key_id] = "#{ENV['AWS_SSH_KEY_ID']}"
+knife[:region] = "us-east-1"
